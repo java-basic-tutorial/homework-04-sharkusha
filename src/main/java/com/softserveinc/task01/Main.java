793f9d91a2d1f69e11a14527df26bf8114ebb43a
sharku <sharku@softserveinc.com>
2022-01-20 11:21:21 +0200

    Uncommented code

--- src/main/java/com/softserveinc/task01/Main.java
+++ src/main/java/com/softserveinc/task01/Main.java
@@ -6,16 +6,23 @@ package com.softserveinc.task01;
 public class Main {
     public static void main(String[] args) {
         // TODO: Uncomment code bellow after the class CoffeeMachineV1 have been created
-//        AbstractCoffeeMachine coffeeMachine = new CoffeeMachineV1(200, 5000, 400);
-//        coffeeMachine.on();
-//        coffeeMachine.addWater(1000);
-//        coffeeMachine.addCoffeeBeans(200);
-//        if (coffeeMachine.makeAmericano()) {
-//            System.out.println("Drink Americano");
-//        }
-//        if (coffeeMachine.makeEspresso()) {
-//            System.out.println("Drink Espresso");
-//        }
-//        coffeeMachine.off();
+        AbstractCoffeeMachine coffeeMachine = new CoffeeMachineV1(200, 5000, 400);
+        System.out.println(coffeeMachine);
+        coffeeMachine.on();
+        System.out.println(coffeeMachine);
+        coffeeMachine.addWater(1000);
+        System.out.println(coffeeMachine);
+        coffeeMachine.addCoffeeBeans(22);
+        System.out.println(coffeeMachine);
+        if (coffeeMachine.makeAmericano()) {
+            System.out.println("Drink Americano");
+        }
+        System.out.println(coffeeMachine);
+        if (coffeeMachine.makeEspresso()) {
+            System.out.println("Drink Espresso");
+        }
+        System.out.println(coffeeMachine);
+        coffeeMachine.off();
+        System.out.println(coffeeMachine);
     }
 }
